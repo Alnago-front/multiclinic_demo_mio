e7bf91a6143e3a605e89c106101c08e792712e45
DevAlnago <dev@alnago.com>
2024-05-02 13:33:14 -0500

    sections iniciales

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-center items-center w-full h-auto bg-blue-700">
+      <div className="flex flex-col max-w-5xl">
+        <section className="flex justify-center items-center w-full h-screen">
+          <h1 className="text-4xl font-bold text-center">
+            Multiclinic1
+          </h1>
+        </section>
+        <section className="flex justify-center items-center w-full h-screen">
+          <h2 className="text-2xl font-bold text-center">
+            Multiclinic2
+          </h2>
+        </section>
+      </div>
+    </main>
+  );
+}
---
+++ src/components/navbar/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <header className="fixed flex justify-center items-center w-full h-18 bg-white">
+      <div className="flex justify-between items-center max-w-5xl w-full">
+        <div>
+          <Link href={'/'}>
+            <Image src="/logos/multiclinic.png" alt="Logo de multiclinic" width={2911} height={883} className="w-36 h-auto" />
+          </Link>
+        </div>
+        <div>
+          <nav className="flex gap-2 p-4">
+            <Link href={'/'} >Opción 1</Link>
+            <Link href={'/'} >Opción 2</Link>
+            <Link href={'/'} >Opción 3</Link>
+            <Link href={'/'} >Opción 4</Link>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
