13dfdcb9e70a2dae84f0b79d6de15da20e2f8f50
Alnago-front <frontd@alnago.com>
2024-05-04 09:32:47 -0500

    colores config

--- src/components/secciones/Galeria.tsx
+++ src/components/secciones/Galeria.tsx
@@ -1,24 +1,21 @@
 import Image from 'next/image'
 import React from 'react'
 import foto from '../../../public/imagenes/2.jpg'
+
 const Galeria = () => {
   return (
-    <div className=" justify-center items-center w-full h-screen bg-cyan-500">
-
-        <div className="bg-black space-y-4">
-
-            <div className="flex justify-center items-center space-x-4">
-                <Image src={foto} alt={'doctor imagen'} className="w-32"/>
-                <Image src={foto} alt={'doctor imagen'} className="w-32"/>
-                <Image src={foto} alt={'doctor imagen'} className="w-32"/>
-            </div>
-            <div className="flex justify-center items-center space-x-4">
-            <Image src={foto} alt={'doctor imagen'} className="w-32"/>
-            <Image src={foto} alt={'doctor imagen'} className="w-32"/>
-            <Image src={foto} alt={'doctor imagen'} className="w-32"/>
-            </div>
-        
-        </div>
+    <div className="flex flex-wrap justify-center gap-10 w-full box-border py-3">    
+            
+              <Image src={foto} alt={'doctor imagen'} className="w-1/5"/>
+              <Image src={foto} alt={'doctor imagen'} className="w-1/5"/>
+              <Image src={foto} alt={'doctor imagen'} className="w-1/5"/>           
+              <Image src={foto} alt={'doctor imagen'} className="w-1/5"/>
+              <Image src={foto} alt={'doctor imagen'} className="w-1/5"/>
+              <Image src={foto} alt={'doctor imagen'} className="w-1/5"/>
+              <Image src={foto} alt={'doctor imagen'} className="w-1/5"/>
+              <Image src={foto} alt={'doctor imagen'} className="w-1/5"/>
+              
+            
     </div>
   )
 }
