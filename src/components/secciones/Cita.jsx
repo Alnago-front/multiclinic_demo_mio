22bc292c308e1a88f56280375755d1d93d7f703e
Alnago-front <frontd@alnago.com>
2024-05-08 13:27:27 -0500

    css info

--- src/components/secciones/Cita.jsx
+++ src/components/secciones/Cita.jsx
@@ -3,13 +3,13 @@ import { motion } from "framer-motion"
 
 const Cita = () => {
   return (
-    <div className='flex flex-col items-center justify-center w-full m-10'>
+    <div className='flex flex-col items-center justify-center w-full my-10'>
 
         <div className='flex flex-col items-center justify-center w-2/4 border-2 border-solid border-principal p-10 gap-20 m-5'>
             <motion.p    initial={{opacity: 0, x: -100}}
                          whileInView={{ opacity: 1, x: 0}}
                          transition={{ ease: "easeOut", duration: 1.3 }}
-                        className='font-mono text-8xl text-center text-segundario'> 
+                        className='font-mono text-3xl text-center text-segundario'> 
 
                         AGENDA TU CITA CON NOSOTROS 
             </motion.p>
