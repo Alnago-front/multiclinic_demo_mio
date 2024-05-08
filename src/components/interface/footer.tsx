c51a83672e6274e17f55e25852e6264090ea5704
DevAlnago <dev@alnago.com>
2024-05-08 13:59:06 -0500

    diseño previo

--- src/components/interface/footer.tsx
+++ src/components/interface/footer.tsx
@@ -1,11 +1,25 @@
+import Image from "next/image";
+
 export default function Footer() {
 
   return (
-    <footer>
-      <div>
-        <h1 className={`${'flex'}`}>Footer</h1>
+    <footer className="flex justify-center items-center w-full h-auto bg-black">
+      <div className="flex flex-row justify-between max-w-5xl py-5">
+
+        <div className="flex justify-center items-center min-w-80">
+          <Image src="/logos/multiclinic.png" alt="Logo de multiclinic" width={2911} height={883} className="w-96 h-auto" />
+        </div>
+
+        <div className="flex justify-center items-center min-w-80">
+          <h1 className="text-white">Nuestra empresa</h1>
+        </div>
+
+        <div className="flex justify-center items-center min-w-80">
+          <h1 className="text-white">Contacto</h1>
+        </div>
+
       </div>
     </footer>
   )
-  
+
 }
