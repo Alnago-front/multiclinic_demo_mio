684e31ebec823f485d69e8ed22c4a7773c36b5f9
DevAlnago <dev@alnago.com>
2024-05-08 14:24:53 -0500

    opciones y diseño navbar

--- src/components/interface/navbar.tsx
+++ src/components/interface/navbar.tsx
@@ -6,11 +6,23 @@ export default function NavBar() {
 
     <nav className="flex absolute justify-center z-10 w-full h-52 bg-gradient-to-b from-black">
 
-        <div className="flex justify-center w-full h-3/6 max-w-5xl">
+      <div className="grid laptop:grid-cols-2 mobile:grid-cols-1 place-content-between w-full h-4/6 max-w-5xl">
+
+        <div className="grid mobile:place-content-center">
           <Link href={'/'}>
             <Image src="/logos/multiclinic.png" alt="Logo de multiclinic" width={2911} height={883} className="w-96 h-auto" />
           </Link>
         </div>
+
+        <div className="place-content-center py-5">
+          <ul className="grid grid-cols-3">
+            <li className="text-white text-center"><Link href={'/'}>Servicios</Link></li>
+            <li className="text-white text-center"><Link href={'/'}>Quienes somos</Link></li>
+            <li className="text-white text-center"><Link href={'/'}>Contacto</Link></li>
+          </ul>
+        </div>
+
+      </div>
 
     </nav>
 
