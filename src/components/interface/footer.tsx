24473017e61387a97a3d4e416e1f3bbac7aa30a7
DevAlnago <dev@alnago.com>
2024-05-14 08:37:10 -0500

    a punto

--- src/components/interface/footer.tsx
+++ src/components/interface/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
 
   return (
     <footer className="flex justify-center items-center w-full h-auto bg-black">
-      <div className="grid desktop:grid-cols-3 mobile:grid-cols-1 w-fullgit  max-w-5xl py-5 pt-3 pb-5">
+      <div className="grid tablet:grid-cols-3 mobile:grid-cols-1 w-fullgit  max-w-5xl pt-3 pb-5">
 
         <div className="flex justify-center items-center min-w-80">
           <Image src="/logos/multiclinic.png" alt="Logo de multiclinic" width={2911} height={883} className="w-96 h-auto" />
--- src/components/interface/navbar.tsx
+++ src/components/interface/navbar.tsx
@@ -5,10 +5,9 @@ export default function NavBar() {
   return (
 
     <nav className="flex absolute justify-center z-10 w-full h-52 bg-gradient-to-b from-black">
-
       <div className="grid laptop:grid-cols-2 mobile:grid-cols-1 place-content-between w-full h-4/6 max-w-5xl">
 
-        <div className="grid mobile:place-content-center">
+        <div className="grid mobile:place-content-center tablet:place-content-start">
           <Link href={'/'}>
             <Image src="/logos/multiclinic.png" alt="Logo de multiclinic" width={2911} height={883} className="w-96 h-auto" priority/>
           </Link>
@@ -23,7 +22,6 @@ export default function NavBar() {
         </div>
 
       </div>
-
     </nav>
 
   )
